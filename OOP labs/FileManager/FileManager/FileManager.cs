110d7ff6bada2dd1fc5195941499d9488e61cc31
dtrygodko <dtrygodko@ukr.net>
2016-03-28 12:12:41 +0300

    added file manager project

---
+++ OOP labs/FileManager/FileManager/FileManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileManager
+{
+    class FileManager
+    {
+        public string CurrentDirectory { get; private set; }
+
+        public FileManager()
+        {
+            CurrentDirectory = Directory.GetCurrentDirectory();
+        }
+
+        public void InfiniteParse()
+        {
+            while(true)
+            {
+
+            }
+        }
+    }
+}
---
+++ OOP labs/FileManager/FileManager/Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileManager manager = new FileManager();
+
+            WriteLine(manager.CurrentDirectory);
+
+            ReadKey();
+        }
+    }
+}
